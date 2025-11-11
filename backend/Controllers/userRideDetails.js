24dd5ad3d9e1da51e567e9c000b1d5beb664e62f
Mohamednoor2453 <mohamednoornoor10@gmail.com>
2025-11-11 13:52:08 +0300

    Added location and geolacation feature to the request ride app, also created the page for home , request ride, and set distination

---
+++ backend/Controllers/userRideDetails.js
@@ -0,0 +1,14 @@
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
+
+const bcrypt = require('bcryptjs');
+const User = require('../Models/users');
+
+
+exports.rideDetails =async (req, res)=>{
+    try {
+        const {}
+    } catch (error) {
+        
+    }
+}
